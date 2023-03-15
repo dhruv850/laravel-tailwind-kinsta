979b63cc259a209d3570a62d31550ac627ea2193
dhruv850 <dhruv850.dp@gmail.com>
2023-03-15 18:12:39 -0400

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{asset('css/app.css')}}" rel="stylesheet"> 
+    <title>Document</title>
+</head>
+<body>
+<div class="max-w-sm  m-24 rounded overflow-hidden shadow-lg">
+  <img class="w-full" src="https://picsum.photos/300/200" alt="Person Image">
+  <div class="px-6 py-4">
+    <div class="font-bold text-xl mb-2">John Doe</div>
+    <p class="text-gray-700 text-base">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque,
+      exercitationem praesentium nihil.
+    </p>
+  </div>
+</div>
+</body>
+</html>
